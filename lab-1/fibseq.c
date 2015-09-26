c089f8f5ecfaf0e1a5ec961f44114306012638dd
jssmile <jssmile19921112@gmail.com>
2015-09-26 12:06:07 +0800

    modify fib.s

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#include <time.h>
 
 extern int fibN(int x);
 
 int main(int argc, char **argv)
 {
+  time_t start_tm, finish_tm; 
+  time(&start_tm); 
   int number=0;
   int result=0;
 
   scanf("%d",&number);
   result = fibN(number);   
   printf("The fibonacci sequence at %d is: %d\n", number, result);
+  time(&finish_tm); 
+  double elapsed_tm=difftime(finish_tm,start_tm); 
+  printf("Recursive for %10.10f seconds.\n", elapsed_tm);
 }
 
 fibN(int x){
